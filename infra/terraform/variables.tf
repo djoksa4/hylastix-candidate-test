7409e15aeb3800550acb14332cb4beb4b227379d
djoksa4 <vodovardjordje.cavps@gmail.com>
2025-09-22 00:21:11 +0200

    initial commit

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+  type        = string
+  description = "Prefix for all resources in the project"
+}
+
+variable "region" {
+  type        = string
+  description = "Azure region for resources"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
